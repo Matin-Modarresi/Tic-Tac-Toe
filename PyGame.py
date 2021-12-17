d02922e8aa9c3b5223f3cd28c79ce6d2adcde966
Matin-Modarresi <modarresimatin@gmail.com>
2021-12-17 16:38:46 +0330

    pygame source implace in main source

--- PyGame.py
+++ PyGame.py
@@ -4,10 +4,11 @@ import pygame.event as GAME_EVENTS
 
 pygame.init()
 windowSize = pygame.display.get_desktop_sizes()
+#windowSize = [(800,800)]
 length = 500
 width = 10
 space=length/6
-
+selected_cell=[]
 mids = [(windowSize[0][0]-length)/2,(windowSize[0][1]-width)/2,(windowSize[0][1]-length)/2,
 		(windowSize[0][0]-width)/2]
 
@@ -27,7 +28,7 @@ for line,i in zip(range(-3,4,2),range(4)):
 #for line,i in zip(range())		
 
 
-def game_board(win,col,row,shape):
+def game_board(win,row,col,shape):
 	global windowSize,length,width,mid_length,mid_width
 
 	for i in range(4):
@@ -36,19 +37,28 @@ def game_board(win,col,row,shape):
 
 	if shape == 1:
 		radius = 60
-		pygame.draw.circle(win,(255,255,255),(cell_ver[row]+space,cell_hor[col]+space),radius,10)
-	else:
+		pygame.draw.circle(win,(255,255,255),(cell_ver[col]+space,cell_hor[row]+space),radius,10)
+	if shape==0:
 		length=30
-		pygame.draw.line(win,(255,255,255),(cell_ver[row]+length,cell_hor[col]+length),(cell_ver[row+1]-length,cell_hor[col+1]-length),15)
-		pygame.draw.line(win,(255,255,255),(cell_ver[row]+length,cell_hor[col+1]-length),(cell_ver[row+1]-length,cell_hor[col]+length),15)
+		pygame.draw.line(win,(255,255,255),(cell_ver[col]+length,cell_hor[row]+length),(cell_ver[col+1]-length,cell_hor[row+1]-length),15)
+		pygame.draw.line(win,(255,255,255),(cell_ver[col]+length,cell_hor[row+1]-length),(cell_ver[col+1]-length,cell_hor[row]+length),15)
 
-	#pygame.draw.rect(win,(9,234,150),(mid_length_hor,cell_hor[1],length,width))
-	#pygame.draw.rect(win,(9,234,150),(mid_length_hor,cell_hor[2],length,width))
-	#pygame.draw.rect(win,(9,234,150),(cell_ver[1],mid_length_ver,width,length))
-	#pygame.draw.rect(win,(9,234,150),(cell_ver[2],mid_length_ver ,width,length))
+
+
+def click_on_cell(win):
+	pass
+	for i in range(3):#cell_hor
+		for j in range(3):#cell_ver
+			if cell_hor[i] < pygame.mouse.get_pos()[1] < cell_hor[i+1] and cell_ver[j] <pygame.mouse.get_pos()[0]< cell_ver[j+1] and pygame.mouse.get_pressed()[0]:
+				if (i,j) in selected_cell:
+					continue
+				else:
+					selected_cell.append((i,j))
+					return i , j
 
 	
-
 	
+
+
 
 	
--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -13,19 +13,8 @@ print(pg.cell_hor)
 print(pg.cell_ver)
 window = pg.pygame.display.set_mode(*pg.windowSize)
 
-
-
-while True:
-
-	pg.game_board(window,0 ,0,0)
-	pg.game_board(window,1 ,2,1)
-
-	for event in pg.GAME_EVENTS.get():
-		if event.type == pg.pygame.KEYDOWN and event.key == pg.pygame.K_ESCAPE:
-			pg.pygame.quit()
-			sys.exit()
-
-	pg.pygame.display.update()
+pg.game_board(window,0,1,-1)
+pg.pygame.display.update()
 
 
 
@@ -33,372 +22,393 @@ while True:
 
 
 
+def print2d(arr):
+	for i in arr:
+		print(*i,sep=' ')
+
+
+
+
+class Node:
+	def __init__(self,map):
+
+		self.leaves=[]
+		self.rows=[]
+		self.value=0
+		self.map=map
+		self.row=0
+		self.col=0
+		self.status = 0
+		
+		self.detail_val ={ 'nozoli':0 , 'soudi':0, 'ofoghi':0 , 'amoodi':0}
+
+
+
+
+	def printTree_bfs(self,file):
+
+		for j in self.leaves:
+
+			file.write(f"{round(j.value,2):<6}"+' '*10)
+			print(f"{round(j.value,2):<6}",end=' '*10)
+		
+		file.write('\n')
+		print()
+
+		for i in self.detail_val:
+			for j in self.leaves:
+		
+				file.write(f"{i:<6}"+str(j.detail_val[i])+' \t')
+				print(f"{i:<6}",j.detail_val[i],end=' \t')
+			print()
+			file.write('\n')
+			
+		for i in range(3):
+			for j in self.leaves:
+				print(*j.map[i],end=' '*11)
+		
+				for letter in j.map[i]:
+					file.write(letter+' ')
+				file.write(' '*10)
+		
+			file.write('\n')
+			print()
+
+
+		
+
+def MAX(self,index=0,row=0,col=0):
+	global last_level,stop_turn
+	stop = stop_condition(self,row,col)
+	
+	self.row = row
+	self.col = col
+
+	
+
+	if stop==1: 
+		self.value/=index
+		return 1
+
+	if stop==-1:
+		self.value/=index
+		return -1
+	
+
+	if index==stop_turn:
+		last_level+=1
+		return 0
+
+
+	map_copy=[]
+	
+	#print2d(self.map)
+	#print()
+	
+	for i in range(3):
+		for j in range(3):
+
+			if self.map[i][j] != '-':
+				continue
+
+			map_copy  = copy.deepcopy(self.map)
+
+
+			if index%2==0:
+				map_copy[i][j]='x'
+			else:
+				map_copy[i][j]='o'
+
+			
+			self.leaves.append(Node(map_copy))
+			MAX(self.leaves[-1],index+1,i,j)
+			
+			#if ignore==1 or ignore==-1:
+			#	break;
+
+
+	if index%2==0:
+		maximum = -1000000*state_turn
+		if len(self.leaves)==0:
+			maximum=0
+
+		for i in self.leaves:	
+			if i.value*state_turn > maximum*state_turn:
+				maximum = i.value
+		
+		self.value+=maximum
+	
+	else:
+		minimum = 1000000*state_turn
+		if len(self.leaves)==0:
+			minimum=0
+
+		for i in self.leaves:
+			if i.value*state_turn < minimum*state_turn:
+				minimum=i.value
+	
+		self.value+=minimum
+
+
+def stop_condition(self,row,col):
+	global state_turn
+	value=0
+	range_  = [
+			   [ [(row,row+3),(col,col+3)]      ,[(row-1,row-3,-1),(col-1,col-3,-1)] ],
+			   [ [(row,row-3,-1),(col,col+3)]   ,[( row+1,row+3),(col-1,col-3,-1)]   ],
+			   [ [(row,row+3),col]				,[(row-1,row-3,-1),col]				 ],
+			   [ [row,(col,col+3)]				,[row,(col-1,col-3,-1)]				 ]
+			  ]
+
+
+	for i in range_:
+		count = 0
+		check = None
+		self.value=0
+		
+
+		for j in i:
+			check_=False
+			if i==range_[0] or i==range_[1]:
+				for n,m in zip(range(*j[0]),range(*j[1])):
+					condition,count,check,check_ = conditions(self,n,m,count,check,check_)
+					if not condition:
+						break
+
+				if i==range_[0]:
+					self.detail_val['nozoli']=self.value
+
+					
+				else:
+					self.detail_val['soudi'] =self.value
+			
+					
+
+			
+			if i==range_[2]:
+				for n in range(*j[0]):
+					condition,count,check,check_ = conditions(self,n,j[1],count,check,check_)
+					if not condition:
+						break
+
+				self.detail_val['amoodi'] =self.value
+	
+				
+
+
+			if i==range_[3]:
+				for m in range(*j[1]):
+					condition,count,check,check_ = conditions(self,j[0],m,count,check,check_)
+					if not condition:
+						break
+
+				self.detail_val['ofoghi'] =self.value
+				
+				
+
+		
+		value+=self.value
+
+		
+
+		if count>=3:
+			self.status = 1
+			self.value+=1000
+			return 1
+
+		if count<=-3:
+			self.status =-1
+			self.value-=1000
+			return -1
+
+	self.value=value
+	return 0
+
+
+
+def conditions(self,row,col,count,check,check_):
+	global state_turn
+	
+	
+	if row>=3 or col>=3 or row<=-1 or col<=-1:
+		return False,count,check,check_
+
+
+	if self.map[row][col]=='-':
+		if not check_:
+			check_=True
+		if check=='x':
+			self.value +=.5*state_turn
+		else:
+			self.value-=.5*state_turn
 
 
 
 
 
+	if self.map[row][col]=='x':
+
+		if check==None or check=='x':
+			if check_:
+				self.value+=1*state_turn
+			else:
+				count+=1*state_turn
+				self.value+=1*state_turn
+				check='x'
+
+		if check=='o':
+			return False,count,check,check_
+
+	
+	
+	if self.map[row][col]=='o':
+
+		if check==None or check=='o':
+			if check_:
+				self.value-=1*state_turn
+			else:
+				count-=1*state_turn
+				self.value-=1*state_turn
+				check='o'
+
+		if check=='x':
+			return False,count,check,check_
+
+	return True,count,check,check_
+
+
+column=None ; row=None
+
+last_level=0
+turn = 0
+stop_turn=5
+stop=0
+
+game_finished = False
 
 
 
+process = open('Process.txt','w')
 
-#def print2d(arr):
-#	for i in arr:
-#		print(*i,sep=' ')
-#
-#
-#
-#
-#class Node:
-#	def __init__(self,map):
-#
-#		self.leaves=[]
-#		self.rows=[]
-#		self.value=0
-#		self.map=map
-#		self.row=0
-#		self.col=0
-#		self.status = 0
-#		
-#		self.detail_val ={ 'nozoli':0 , 'soudi':0, 'ofoghi':0 , 'amoodi':0}
-#
-#
-#
-#
-#	def printTree_bfs(self,file):
-#
-#		for j in self.leaves:
-#
-#			file.write(f"{round(j.value,2):<6}"+' '*10)
-#			print(f"{round(j.value,2):<6}",end=' '*10)
-#		
-#		file.write('\n')
-#		print()
-#
-#		for i in self.detail_val:
-#			for j in self.leaves:
-#		
-#				file.write(f"{i:<6}"+str(j.detail_val[i])+' \t')
-#				print(f"{i:<6}",j.detail_val[i],end=' \t')
-#			print()
-#			file.write('\n')
-#			
-#		for i in range(3):
-#			for j in self.leaves:
-#				print(*j.map[i],end=' '*11)
-#		
-#				for letter in j.map[i]:
-#					file.write(letter+' ')
-#				file.write(' '*10)
-#		
-#			file.write('\n')
-#			print()
-#
-#
-#		
-#
-#def MAX(self,index=0,row=0,col=0):
-#	global last_level,stop_turn
-#	stop = stop_condition(self,row,col)
-#	
-#	self.row = row
-#	self.col = col
-#
-#	
-#
-#	if stop==1: 
-#		self.value/=index
-#		return 1
-#
-#	if stop==-1:
-#		self.value/=index
-#		return -1
-#	
-#
-#	if index==stop_turn:
-#		last_level+=1
-#		return 0
-#
-#
-#	map_copy=[]
-#	
-#	#print2d(self.map)
-#	#print()
-#	
-#	for i in range(3):
-#		for j in range(3):
-#
-#			if self.map[i][j] != '-':
-#				continue
-#
-#			map_copy  = copy.deepcopy(self.map)
-#
-#
-#			if index%2==0:
-#				map_copy[i][j]='x'
-#			else:
-#				map_copy[i][j]='o'
-#
-#			
-#			self.leaves.append(Node(map_copy))
-#			MAX(self.leaves[-1],index+1,i,j)
-#			
-#			#if ignore==1 or ignore==-1:
-#			#	break;
-#
-#
-#	if index%2==0:
-#		maximum = -1000000*state_turn
-#		if len(self.leaves)==0:
-#			maximum=0
-#
-#		for i in self.leaves:	
-#			if i.value*state_turn > maximum*state_turn:
-#				maximum = i.value
-#		
-#		self.value+=maximum
-#	
-#	else:
-#		minimum = 1000000*state_turn
-#		if len(self.leaves)==0:
-#			minimum=0
-#
-#		for i in self.leaves:
-#			if i.value*state_turn < minimum*state_turn:
-#				minimum=i.value
-#	
-#		self.value+=minimum
-#
-#
-#def stop_condition(self,row,col):
-#	global state_turn
-#	value=0
-#	range_  = [
-#			   [ [(row,row+3),(col,col+3)]      ,[(row-1,row-3,-1),(col-1,col-3,-1)] ],
-#			   [ [(row,row-3,-1),(col,col+3)]   ,[( row+1,row+3),(col-1,col-3,-1)]   ],
-#			   [ [(row,row+3),col]				,[(row-1,row-3,-1),col]				 ],
-#			   [ [row,(col,col+3)]				,[row,(col-1,col-3,-1)]				 ]
-#			  ]
-#
-#
-#	for i in range_:
-#		count = 0
-#		check = None
-#		self.value=0
-#		
-#
-#		for j in i:
-#			check_=False
-#			if i==range_[0] or i==range_[1]:
-#				for n,m in zip(range(*j[0]),range(*j[1])):
-#					condition,count,check,check_ = conditions(self,n,m,count,check,check_)
-#					if not condition:
-#						break
-#
-#				if i==range_[0]:
-#					self.detail_val['nozoli']=self.value
-#
-#					
-#				else:
-#					self.detail_val['soudi'] =self.value
-#			
-#					
-#
-#			
-#			if i==range_[2]:
-#				for n in range(*j[0]):
-#					condition,count,check,check_ = conditions(self,n,j[1],count,check,check_)
-#					if not condition:
-#						break
-#
-#				self.detail_val['amoodi'] =self.value
-#	
-#				
-#
-#
-#			if i==range_[3]:
-#				for m in range(*j[1]):
-#					condition,count,check,check_ = conditions(self,j[0],m,count,check,check_)
-#					if not condition:
-#						break
-#
-#				self.detail_val['ofoghi'] =self.value
-#				
-#				
-#
-#		
-#		value+=self.value
-#
-#		
-#
-#		if count>=3:
-#			self.status = 1
-#			self.value+=1000
-#			return 1
-#
-#		if count<=-3:
-#			self.status =-1
-#			self.value-=1000
-#			return -1
-#
-#	self.value=value
-#	return 0
-#
-#
-#
-#def conditions(self,row,col,count,check,check_):
-#	global state_turn
-#	
-#	
-#	if row>=3 or col>=3 or row<=-1 or col<=-1:
-#		return False,count,check,check_
-#
-#
-#	if self.map[row][col]=='-':
-#		if not check_:
-#			check_=True
-#		if check=='x':
-#			self.value +=.5*state_turn
-#		else:
-#			self.value-=.5*state_turn
-#
-#
-#
-#
-#
-#	if self.map[row][col]=='x':
-#
-#		if check==None or check=='x':
-#			if check_:
-#				self.value+=1*state_turn
-#			else:
-#				count+=1*state_turn
-#				self.value+=1*state_turn
-#				check='x'
-#
-#		if check=='o':
-#			return False,count,check,check_
-#
-#	
-#	
-#	if self.map[row][col]=='o':
-#
-#		if check==None or check=='o':
-#			if check_:
-#				self.value-=1*state_turn
-#			else:
-#				count-=1*state_turn
-#				self.value-=1*state_turn
-#				check='o'
-#
-#		if check=='x':
-#			return False,count,check,check_
-#
-#	return True,count,check,check_
-#
-#
-#column=None ; row=None
-#
-#last_level=0
-#turn = 0
-#stop_turn=6
-#stop=0
-#
-#game_finished = False
-#
-#
-#
-#process = open('Process.txt','w')
-#
-#
-#
-#state_turn=-1
-#state_difficulty = -1
-#	
-#
-#while not game_finished:
-#	
-#	tree=Node(game_board)
-#	MAX(tree,turn)
-#
-#
-#	if turn%2==1:	
-#		maximum=-1000000*state_difficulty
-#		
-#		for i,index in zip(tree.leaves,range(len(tree.leaves))):
-#			if i.value*state_difficulty>maximum*state_difficulty:
-#				maximum=i.value
-#				index_max=index
-#			
-#		
-#		process = open('Process.txt','a',buffering=1)
-#		
-#		process.write('index_max '+str(index_max)+'\n')
-#		process.write(str(tree.leaves[index_max].row)+ ' ' +str(tree.leaves[index_max].col)+'\n')
-#
-#	
-#
-#		process.write('\n')
-#
-#		for i in tree.leaves[index_max].map:
-#			for j in i:
-#				process.write(str(j)+' ')
-#			process.write('\n')
-#
-#		process.write('\n')
-#		
-#		print()
-#		print(index_max)
-#		
-#		print(tree.leaves[index_max].row,tree.leaves[index_max].col)
-#		
-#		print2d(tree.leaves[index_max].map)
-#
-#		if tree.leaves[index_max].status==1:
-#			print("you lose")
-#			process.close()
-#			game_finished = True
-#			break
-#
-#		print()
-#		process.write('\n')
-#
-#		tree.printTree_bfs(process)
-#		
-#		print()
-#
-#		process.write('\n')
-#
-#		tree.leaves[index_max].printTree_bfs(process)
-#
-#		process.write('\n'+'#'*165+'\n')
-#
-#		print()
-#		
-#		tree=tree.leaves[index_max]
-#
-#	else:
-#		cor.gotoxy(0,0)
-#		print('select a column: ',end='')
-#
-#		
-#		column=int(input())
-#		
-#
-#		
-#		os.system('cls')
-#
-#		if tree.leaves[column].status == -1:
-#			print('you win')
-#			process.close()
-#			#esm.send_email('Process.txt','Process.txt',0)
-#
-#			game_finished = True
-#			break
-#
-#		tree=tree.leaves[column]		
-#
-#	game_board=copy.deepcopy(tree.map)
-#	turn+=1
-#
-#	stop_turn=6+turn
+
+
+state_turn=-1
+change_turn = 0 if state_turn is 1 else 1
+state_difficulty = 1
+	
+
+while not game_finished:
+	
+	tree=Node(game_board)
+	MAX(tree,turn)
+
+
+	if turn%2==change_turn:	
+		maximum=-1000000*state_difficulty
+		
+		for i,index in zip(tree.leaves,range(len(tree.leaves))):
+			if i.value*state_difficulty>maximum*state_difficulty:
+				maximum=i.value
+				index_max=index
+
+
+		pg.selected_cell.append((tree.leaves[index_max].row,tree.leaves[index_max].col))
+		pg.game_board(window,tree.leaves[index_max].row,tree.leaves[index_max].col,0)
+		pg.pygame.display.update()
+
+
+		process = open('Process.txt','a',buffering=1)
+		
+		process.write('index_max '+str(index_max)+'\n')
+		process.write(str(tree.leaves[index_max].row)+ ' ' +str(tree.leaves[index_max].col)+'\n')
+
+	
+
+		process.write('\n')
+
+		for i in tree.leaves[index_max].map:
+			for j in i:
+				process.write(str(j)+' ')
+			process.write('\n')
+
+		process.write('\n')
+		
+		print()
+		print(index_max)
+		
+		print(tree.leaves[index_max].row,tree.leaves[index_max].col)
+		
+		print2d(tree.leaves[index_max].map)
+
+		if tree.leaves[index_max].status==1:
+			print("you lose")
+			process.close()
+			game_finished = True
+			break
+
+		if tree.status==0 and len(tree.leaves)==1:
+			print('draw')
+			game_finished=True
+			break
+
+		print()
+		process.write('\n')
+
+		tree.printTree_bfs(process)
+		
+		print()
+
+		process.write('\n')
+
+		tree.leaves[index_max].printTree_bfs(process)
+
+		process.write('\n'+'#'*165+'\n')
+
+		print()
+		
+		tree=tree.leaves[index_max]
+
+	else:
+		
+		column=None
+
+		while column==None:
+
+			a = pg.click_on_cell(window)
+			if a is not None:
+				print(a)
+				pg.game_board(window,a[0] ,a[1],1)
+
+				for i,index in zip(tree.leaves , range(len(tree.leaves))):
+					if i.row==a[0] and i.col==a[1]:
+						column = index
+			
+			for event in pg.GAME_EVENTS.get():
+				if event.type == pg.pygame.KEYDOWN and event.key == pg.pygame.K_ESCAPE:
+					pg.pygame.quit()
+					sys.exit()
+
+			pg.pygame.display.update()
+
+		os.system('cls')
+
+		if tree.leaves[column].status == -1:
+			print('you win')
+			process.close()
+			game_finished = True
+			break
+
+		if tree.status==0 and len(tree.leaves)==1:
+			print('draw')
+			game_finished=True
+			break
+			#esm.send_email('Process.txt','Process.txt',0)
+
+
+		tree=tree.leaves[column]		
+
+	game_board=copy.deepcopy(tree.map)
+	turn+=1
+
+	stop_turn=5+turn
+	
